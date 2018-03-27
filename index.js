c40588caaa3b10d4c98ed6269f0e3cb4a57baf31
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-03-27 22:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,16 +22,16 @@ function viewCart() {
   if ( cart.length === 0 ) {
     return "Your shopping cart is empty."
   } else if ( cart.length === 1 ) {
-    return `In your cart, you have  ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var array = []
     for ( var i = 0; i < cart.length -1; i++) {
-    array.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    array.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
     }
     array.push(` and ${cart[cart.length -1].itemName} at $${cart[cart.length - 1].itemPrice}`)
-    return "In your cart, you have" + array
+    return "In your cart, you have " + array
   }
  }
 
